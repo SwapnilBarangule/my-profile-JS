d45ad7341b68780319ac480775a6577e67fd1999
SwapnilBarangule <swapnilbarangule25@gmail.com>
2023-08-08 19:19:58 +0530

    AssignmentOP
    Committe to main

---
+++ 05_assignmentOperatio.js
@@ -0,0 +1,45 @@
+console.log(`---------Assignment Op-------`);
+var num1 =10;
+num1+=5;
+console.log(`Result is: ${num1}`);
+num1-=5;
+console.log(`Result is:${num1}`);
+num1*=5;
+console.log(`Result is:${num1}`);
+
+
+console.log(`------Comparison Op------`);
+var num1=20;
+var num2= "20";
+var result = num1==num2;
+console.log(`Result is: ${result}`);
+console.log(`result ${result}`)
+console.log(`== Comparison ${47 =='47'}`);
+console.log(`== Comparison ${47 ==74}`);
+
+var result = num1 === num2;
+console.log(`=== Comparison ${"47"==='47'}`); 
+
+ var reault = num1 != num2;
+ console.log(`using != result is: ${result}`);
+
+ var reault = num1 !== num2;
+ console.log(`using !== result is: ${result}`);
+
+ var num3 = 30;
+ var num4 = 29;
+var result = num3 < num4;
+console.log(`using < operator result is: ${result}`);
+
+result = num3 > num4;
+console.log(`using > operator result is: ${result}`);
+
+num5 = 30;
+var result = num3 < num5;
+console.log(`using < operator result is: ${result}`);
+
+num5 = 30;
+var result = num3 <= num5;
+console.log(`using <= operator result is: ${result}`);
+
+console.log(`-------Logical Op------`);
